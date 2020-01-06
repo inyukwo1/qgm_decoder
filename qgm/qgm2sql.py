9d113562d8f59e8383b45942c2b8c503e4a2f6cb
hkkang <hkkang@dblab.postech.ac.kr>
2020-01-06 13:04:42 +0900

    change design

--- qgm/qgm2sql.py
+++ qgm/qgm2sql.py
@@ -1,11 +1,87 @@
 import json
 import argparse
+from ops import WHERE_OPS, AGG_OPS, BOX_OPS
 
 
-def qgm2sql(qgm, schema):
-    pass
+def decode_qgm_box(boxes, schema):
+    # Construct from clause
+    # get quantifiers with type f
+    # get quantifiers with type s and has no predicate
+    # get all the join predicates between quantifiers with type f
+    # assumption is that if there is a quantifier with type s for the from clause, there are no other quantifiers in the box for the from clause.
+    # set dictioanry for alias
+
+    select_box = group_box = order_box = None
+    for box in boxes:
+        if box['operator'] == BOX_OPS.index('select'):
+            select_box = box
+        elif box['operator'] == BOX_OPS.index('groupBy'):
+            group_box = box
+        elif box['operator'] in [BOX_OPS.index(key) for key in ['orderByDesc', 'orderByAsc']]:
+            order_box = box
+
+    # Construct from clause
+    tables = []
+    alias = {}
+    for idx in range(len(select_box['body']['quantifiers'])):
+        quantifier = select_box['body']['quantifiers'][idx]
+        quantifier_type = select_box['body']['quantifier_types'][idx]
+
+        if quantifier_type == 'f':
+            table_name = schema['table_names'][quantifier]
+            tables += [table_name]
+            alias[table_name] = len(alias) + 1
+    if not tables:
+        assert len(select_box['body']['quantifiers']) == 1
+        tables += [decode_qgm_box(select_box['body']['quantifiers'])]
+
+    join_conditions = []
+    if len(tables) > 1:
+        for idx in range(len(select_box['body']['join_predicates'])):
+            table1_name = select_box['body']['join_predicates'][idx]
+
+            pass
+
+    # Construct where clause
+    # get all local predicates and join predeicates with quantifier type s
+
+    # construct group by clause
+    # if box with group by operator
+
+    # Construct having clause
+    # if predicates in the box with group by operator
+
+    # Construct order by clause
+    # if box with order by operator
+
+    # Construct select clause
+    # read in head of box with operator select
+
+    # Intersect, union, except
+    # read in head of box with operator other than select
 
     return None
+
+def qgm2sql(iidx, info, qgm, schema):
+    print(iidx)
+    key = None
+    s_idx = None
+    for idx, box in enumerate(qgm):
+        if box['operator'] in [BOX_OPS.index(key) for key in ['intersect', 'union', 'except']]:
+            s_idx = idx
+            key = BOX_OPS[box['operator']]
+    if s_idx:
+        q1 = decode_qgm_box(qgm[:s_idx], schema)
+        q2 = decode_qgm_box(qgm[s_idx:], schema)
+        query = q1 + key.upper() + q2
+    else:
+        query = decode_qgm_box(qgm, schema)
+
+    # Debugging
+    print(query == info['query'])
+
+    return query
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -16,9 +92,7 @@ if __name__ == '__main__':
 
     # Load DB info
     dbs = json.load(open(args.db, 'rb'))
-    db_dic = {}
-    for db in dbs:
-        db_dic[db['db_id']] = db
+    db_dic = {db['db_id']: db for db in dbs}
 
     # Load data
     datas = json.load(open(args.source, 'rb'))
@@ -26,12 +100,14 @@ if __name__ == '__main__':
     print('Loading complete!')
 
     # Convert
-    tmp = [qgm2sql(data, db_dic[data['db_id']]) for data in datas]
+    tmp = [qgm2sql(idx, data, data['qgm'], db_dic[data['db_id']]) for idx, data in enumerate(datas)]
 
     print('Translating complete!')
 
     # Save
-    with open(args.destin, 'wb') as f:
-        json.dump(f, tmp)
+    with open(args.destin, 'w') as f:
+        json.dump(tmp, f)
 
     print('Saving QGM complete!')
+
+
--- qgm/sql2qgm.py
+++ qgm/sql2qgm.py
@@ -1,12 +1,149 @@
 import json
 import argparse
-from process_sql import get_sql
+from ops import WHERE_OPS, AGG_OPS, BOX_OPS
 
-WHERE_OPS = ['not', 'between', '=', '>', '<', '>=', '<=', '!=', 'in', 'like', 'is', 'exists']
-AGG_OPS = ['none', 'max', 'min', 'count', 'sum', 'avg']
+def create_group_by_box(sql, info, schema):
+    # Append group by box
+    heads = []
+    quantifiers = []
+    quantifier_types = []
+    local_predicates = []
+    if info['groupBy']:
+        for group_unit in info['groupBy']:
+            # Get heads
+            agg_id = group_unit[0]
+            col_id = group_unit[1]
+            heads += [(agg_id, col_id)]
+
+            # Get body - quantifiers
+            table_id = schema['column_names'][col_id][0]
+            quantifiers += [table_id]
+            quantifier_types += ['f']
+
+        for idx in range(0, len(info['having']), 2):
+            having_unit = info['having'][idx]
+            operator = having_unit[1]
+            agg_id = having_unit[2][1][0]
+            col_id = having_unit[2][1][1]
+            if col_id != 0:
+                table_id = schema['column_names'][col_id][0]
+                quantifiers += [table_id]
+                quantifier_types += ['f']
+            right_operand = having_unit[3]
+            local_predicates += [(agg_id, col_id, operator, right_operand)]
+        body = {'quantifiers': quantifiers, 'quantifier_types': quantifier_types,
+                'join_predicates': None, 'local_predicates': local_predicates}
+        box = {'head': heads, 'body': body, 'operator': BOX_OPS.index('groupBy')}
+        return box
+    else:
+        return None
 
 
-def sql2qgm(iidx, sql, info, schema):
+def create_order_by_box(sql, info, schema, group_by_box):
+    heads = []
+    quantifiers = []
+    quantifier_types = []
+    if info['orderBy']:
+        order_type = info['orderBy'][0]
+        operator = 'orderByDesc' if order_type == 'desc' else 'orderByAsc'
+        for order_unit in info['orderBy'][1]:
+            # Get info
+            agg_id = order_unit[1][0]
+            col_id = order_unit[1][1]
+            heads += [(agg_id, col_id)]
+            if col_id == 0:
+                table_id = group_by_box['body']['quantifiers'][0]
+                quantifiers += [table_id]
+                quantifier_types += [group_by_box['body']['quantifier_types'][0]]
+            else:
+                table_id = schema['column_names'][col_id][0]
+                quantifiers += [table_id]
+                quantifier_types += ['f']
+        # Create body
+        body = {'quantifiers': quantifiers, 'quantifier_types': quantifier_types,
+                'join_predicates': None, 'local_predicates': None}
+        # Create box
+        box = {'head': heads, 'body': body, 'operator': BOX_OPS.index(operator)}
+        return box
+    else:
+        return None
+
+
+def create_select_box(sql, info, schema):
+    heads = []
+    quantifiers = []
+    quantifier_types = []
+    local_predicates = []
+    join_predicates = []
+
+    # Create head - select agg and columns
+    for select_unit in info['select'][1]:
+        agg = select_unit[0]
+        col_id = select_unit[1][1][1]
+        heads += [(agg, col_id)]
+
+    # Create body - quantifiers
+    for table_unit in info['from']['table_units']:
+        assert table_unit[0] in ['sql', 'table_unit']
+        if 'sql' == table_unit[0]:
+            something = sql2qgm(sql, table_unit[1], schema)
+            quantifiers += [something]
+            quantifier_types += ['s']
+        else:
+            table_id = table_unit[1]
+            quantifiers += [table_id]
+            quantifier_types += ['f']
+
+    # Create body - join predicates
+    for conds_unit in info['from']['conds']:
+        something = conds_unit
+        # left col
+        col1_id = something[2][1][1]
+        table1_id = schema['column_names'][col1_id][0]
+        assert table1_id in quantifiers
+
+        # right col
+        col2_id = something[3][1]
+        table2_id = schema['column_names'][col2_id][0]
+        assert table2_id in quantifiers
+
+        # join
+        join_predicates += [((col1_id), WHERE_OPS.index('='), (col2_id))]
+
+    for idx in range(0, len(info['where']), 2):
+        where_unit = info['where'][idx]
+
+        # Agg
+        agg_id = where_unit[2][1][0]
+
+        # Operand1
+        col_id = where_unit[2][1][1]
+        table_id = schema['column_names'][col_id][0]
+        assert table_id in quantifiers
+
+        # Operator
+        operator = where_unit[1]
+
+        # Operand2
+        if isinstance(where_unit[3], dict):
+            right_operand = sql2qgm(sql, where_unit[3], schema)
+            quantifiers += [right_operand]
+            quantifier_types += ['s']
+        else:
+            right_operand = where_unit[3]
+
+        local_predicates += [(agg_id, col_id, operator, right_operand)]
+
+    # Combine body
+    body = {'quantifiers': quantifiers, 'quantifier_types': quantifier_types,
+                'join_predicates': join_predicates, 'local_predicates': local_predicates}
+    # Create Box
+    box = {'head': heads, 'body': body, 'operator': BOX_OPS.index('select')}
+
+    return box
+
+
+def sql2qgm(sql, info, schema):
     '''
     box unit:
         1. table
@@ -18,8 +155,10 @@ def sql2qgm(iidx, sql, info, schema):
     box_body: {'quantifiers', 'quantifier_types', 'join_predicates', 'local_predicates'}
     box_head: []
     '''
+    select_box = create_select_box(sql, info, schema)
+    group_by_box = create_group_by_box(sql, info, schema)
+    order_by_box = create_order_by_box(sql, info, schema, group_by_box)
 
-    # Choose Box Operator
     if info['intersect']:
         box_operator = 'intersect'
     elif info['union']:
@@ -29,92 +168,16 @@ def sql2qgm(iidx, sql, info, schema):
     else:
         box_operator = 'select'
 
-    quantifiers = []
-    quantifier_types = []
-    local_predicates = []
-    join_predicates = []
+    # Combine boxes
+    boxes = [select_box]
+    if group_by_box:
+        boxes += [group_by_box]
+    if order_by_box:
+        boxes += [order_by_box]
 
-    # Create Box - body - quantifiers
-    for table_unit in info['from']['table_units']:
-        assert table_unit[0] in ['sql', 'table_unit']
-        if 'sql' == table_unit[0]:
-            something = sql2qgm(iidx, sql, table_unit[1], schema)
-            quantifiers += [something]
-            quantifier_types += ['s']
-        else:
-            table_id = table_unit[1]
-            table_name = schema['table_names'][table_id]
-            quantifiers += [table_id]
-            quantifier_types += ['f']
-
-
-    # Create Box - body - join predicates
-    for conds_unit in info['from']['conds']:
-        something = conds_unit
-        # 1
-        col1_id = something[2][1][1]
-        table_id = schema['column_names'][col1_id][0]
-        table_name = schema['table_names'][table_id]
-        try:
-            quantifier1_id = quantifiers.index(table_id)
-        except:
-            print(iidx)
-            return []
-
-        # 2
-        col2_id = something[3][1]
-        table_id = schema['column_names'][col2_id][0]
-        table_name = schema['table_names'][table_id]
-        try:
-            quantifier2_id = quantifiers.index(table_id)
-        except:
-            print(iidx)
-            stop = 1
-
-        join_predicates += [((quantifier1_id, col1_id), WHERE_OPS.index('='), (quantifier2_id, col2_id))]
-
-    for idx in range(0, len(info['where']), 2):
-        where_unit = info['where'][idx]
-
-        # Operand1
-        col_id = where_unit[2][1][1]
-        table_id = schema['column_names'][col_id][0]
-        table_name = schema['table_names'][table_id]
-        try:
-            quantifier_id = quantifiers.index(table_id)
-        except:
-            print(iidx)
-            return []
-
-        # Operator
-        operator = where_unit[1]
-
-        # Operand2
-        if isinstance(where_unit[3], dict):
-            right_operand = sql2qgm(iidx, sql, where_unit[3], schema)
-            quantifiers += [right_operand]
-            quantifier_types += ['s']
-        else:
-            right_operand = where_unit[3]
-
-        local_predicates += [(quantifier_id, operator, right_operand)]
-
-    heads = []
-    # Create Box - head - select agg and columns
-    for select_unit in info['select'][1]:
-        agg = select_unit[0]
-        col_id = select_unit[1][1][1]
-        heads += [(agg, col_id)]
-
-    # Create Box1
-    body = {'quantifiers': quantifiers, 'quantifier_types': quantifier_types,
-                'join_predicates': join_predicates, 'local_predicates': local_predicates}
-    box1 = {'head': heads, 'body': body, 'operator': box_operator}
-
-    boxes = [box1]
-    # Create Box2
+    # Create box2
     if box_operator != 'select':
-        box2 = sql2qgm(iidx, sql, info[box_operator], schema)
+        box2 = sql2qgm(sql, info[box_operator], schema)
         boxes += box2
 
     return boxes
@@ -129,9 +192,7 @@ if __name__ == '__main__':
 
     # Load DB info
     dbs = json.load(open(args.db, 'rb'))
-    db_dic = {}
-    for db in dbs:
-        db_dic[db['db_id']] = db
+    db_dic = {db['db_id']: db for db in dbs}
 
     # Load data
     datas = json.load(open(args.source, 'rb'))
@@ -140,7 +201,7 @@ if __name__ == '__main__':
 
     # Convert
     for idx, data in enumerate(datas):
-        tmp = sql2qgm(idx, data['query'], data['sql'], db_dic[data['db_id']])
+        tmp = sql2qgm(data['query'], data['sql'], db_dic[data['db_id']])
         data['qgm'] = tmp
 
     print('Translating complete!')
